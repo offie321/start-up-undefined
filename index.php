8f6f6c51c2b3ae103390afefcbc486e741099fe1
offie321 <43699906+offie321@users.noreply.github.com>
2019-05-08 08:44:37 +0200

    bootstrap verwijderd

--- index.php
+++ index.php
@@ -19,9 +19,6 @@ if ($conn->connect_error) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
     <!-- Font awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
@@ -31,69 +28,11 @@ if ($conn->connect_error) {
   <body>
 <!-- nav -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">Gaming</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login <i class="fas fa-sign-in-alt"></i></button>
-    </form>
-  </div>
 </nav>
 
 <!-- slideshow -->
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/leagueoflegends.jpg" width="100%" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>League of Legends</h5>
-        <p>League of Legends is een MOBA (multiplayer online battle arena)</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="img/rocketleague.jpg" width="100%" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Lorem Ipsum</h5>
-        <p>...</p>
-      </div>
-    </div>
-    <div class="carousel-item">
-      <img src="img/minecraft.jpg" width="100%" alt="...">
-      <div class="carousel-caption d-none d-md-block">
-        <h5>Lorem Ipsum</h5>
-        <p>...</p>
-      </div>
-    </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
-</div>
 
 <br>
 
