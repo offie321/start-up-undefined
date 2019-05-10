ac306aa3920f759eec0b8b90af51b1fb627209f1
offie321 <43699906+offie321@users.noreply.github.com>
2019-05-10 14:07:08 +0200

    add tabs

--- ingelogd.php
+++ ingelogd.php
@@ -1,3 +1,5 @@
+<!-- dashboard page -->
+
 <?php
 session_start();
 if ($_SESSION['ingelogd'] != true) {
@@ -39,9 +41,57 @@ if (isset($_SESSION['user'])) {
 
      </header>
      <div class="spacer"></div>
+     <div class="spacer"></div>
 
-     <?php echo "<h1> Welkom". " ".$_SESSION['user']." " ."op deze pagina </h1>" ?>
 
-     <a class="uitlog-knop" href="logout.php">uitloggen</a>
+<div class="boxbtns">
+<button class="tablink hover-underline-animation" onclick="openPage('Home', this, '#222222')" id="defaultOpen">Home</button>
+<button class="tablink hover-underline-animation" onclick="openPage('News', this, '#222222')">Add Item</button>
+<button class="tablink hover-underline-animation" onclick="openPage('Contact', this, '#222222')">Contact</button>
+<button class="tablink hover-underline-animation" onclick="openPage('About', this, '#222222')">About</button>
+</div>
+<div id="Home" class="tabcontent">
+  <h3>Home</h3>
+  <?php echo "<h1> Welkom". " ".$_SESSION['user']." " ."op deze pagina </h1>" ?>
+  <br>
+  <a class="uitlog-knop" href="logout.php">uitloggen</a>
+</div>
+
+<div id="News" class="tabcontent">
+  <h3>Add new Items</h3>
+  <p>WURD UN GEWURKT</p>
+</div>
+
+<div id="Contact" class="tabcontent">
+  <h3>.....</h3>
+  <p>Get in touch, or swing by for a cup of coffee.</p>
+</div>
+
+<div id="About" class="tabcontent">
+  <h3>....</h3>
+  <p>Who we are and what we do.</p>
+</div>
+
+
+
+
+<script>
+  function openPage(pageName,elmnt,color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    elmnt.style.backgroundColor = color;
+  }
+
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+</script>
    </body>
  </html>
